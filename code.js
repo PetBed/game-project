5a036503df1a0c37127409d47e4578a7a5a8f3ee
Pet_Bed <roti_canai_telur@outlook.com>
2022-08-25 20:11:42 +0800

    added efficient code

--- code.js
+++ code.js
@@ -1,16 +1,24 @@
-var p_1 = document.getElementById("mecury")
-var p_2 = document.getElementById("venus")
-var p_3 = document.getElementById("earth")
-var p_4 = document.getElementById("mars")
-var p_5 = document.getElementById("jupiter")
-var p_6 = document.getElementById("saturn")
-var p_7 = document.getElementById("uranus")
-var p_8 = document.getElementById("neptune")
-var p_9 = document.getElementById("pluto")
+var mecury = document.getElementById("mecury")
+var venus = document.getElementById("venus")
+var earth = document.getElementById("earth")
+var mars = document.getElementById("mars")
+var jupiter = document.getElementById("jupiter")
+var saturn = document.getElementById("saturn")
+var uranus = document.getElementById("uranus")
+var neptune = document.getElementById("neptune")
+var pluto = document.getElementById("pluto")
 
-var planetLeft = -40;
-for (i = 0; i < 10; i++) {
-    i++
-    planetLeft -= 40;
-    
+const planets = [mecury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
+//console.log(planets)
+
+function positionPlanets() {
+    var planetLeft = -50;
+    for (i = 0; i < 9; i++) {
+        var planet = planets[i]
+        planet.style.left = planetLeft + "px";
+        planet.style.transformOrigin = Math.abs(planetLeft)+100+"px";
+        console.log(Math.abs(planetLeft)+100 + planetLeft)
+        planetLeft += -50;
+    }
 }
+positionPlanets();
