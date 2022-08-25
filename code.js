b4390da4c8f66c965c867323d06c631029e89e98
Pet_Bed <roti_canai_telur@outlook.com>
2022-08-25 19:41:33 +0800

    init

---
+++ code.js
@@ -0,0 +1,16 @@
+var p_1 = document.getElementById("mecury")
+var p_2 = document.getElementById("venus")
+var p_3 = document.getElementById("earth")
+var p_4 = document.getElementById("mars")
+var p_5 = document.getElementById("jupiter")
+var p_6 = document.getElementById("saturn")
+var p_7 = document.getElementById("uranus")
+var p_8 = document.getElementById("neptune")
+var p_9 = document.getElementById("pluto")
+
+var planetLeft = -40;
+for (i = 0; i < 10; i++) {
+    i++
+    planetLeft -= 40;
+    
+}
